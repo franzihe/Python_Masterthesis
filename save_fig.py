941fba46e1315cd683c3ad429c87a3b45aadf0ec
franzihe <franzihe@student.geo.uio.no>
2018-01-21 23:02:56 +0100

    added landscape saving a4

--- save_fig.py
+++ save_fig.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 # In[ ]:
 
 ### Save Figure
-def save_figure_landscape(directory, figure_name, form):
+def save_figure_landscape_a4(directory, figure_name, form):
     plt.savefig('%s/%s' % (directory, figure_name), orientation = 'landscape',
            papertype = 'a4', format = form)
 
@@ -20,3 +20,6 @@ def save_figure_portrait(directory, figure_name):
     plt.savefig('%s/%s' % (directory, figure_name), orientation = 'portrait',
            papertype = 'a4', format = form )
 
+
+def save_figure_landscape(directory, figure_name, form):
+    plt.savefig('%s/%s' % (directory, figure_name), orientation = 'landscape', format = form)
