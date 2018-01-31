63e3688ae5534af1ae5647851a5fc1035664c866
franzihe <franzihe@student.geo.uio.no>
2018-01-31 16:39:16 +0100

    includes portrait and landscape a4 and not a4

--- save_fig.py
+++ save_fig.py
@@ -16,9 +16,12 @@ def save_figure_landscape_a4(directory, figure_name, form):
 
 # In[ ]:
 
-def save_figure_portrait(directory, figure_name,form):
+def save_figure_portrait_a4(directory, figure_name,form):
     plt.savefig('%s/%s' % (directory, figure_name), orientation = 'portrait',
            papertype = 'a4', format = form )
+
+def save_figure_portrait(directory, figure_name,form):
+    plt.savefig('%s/%s' % (directory, figure_name), orientation = 'portrait', format = form )
 
 
 def save_figure_landscape(directory, figure_name, form):
