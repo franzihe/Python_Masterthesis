4d15b73915b8660300f628791bbc73dc5dd60a84
franzihe <franzihe@student.geo.uio.no>
2018-02-03 22:57:40 +0100

    included plots similar to Mortens

--- MEPS/plot_sfc_spaghetti.py
+++ MEPS/plot_sfc_spaghetti.py
@@ -65,9 +65,53 @@ def spaghetti_sfc_dofe(lead_time_sfc, variable, dofence_60, time_sfc, Xmax, day,
     ax.set_title(title, fontsize=30, color =blue )
 # tight layout
     plt.tight_layout()
-        
-
-    
-        
 
 
+
+
+def spaghetti_sfc_dofe_Morten(lead_time_sfc, variable, dofence_60, time_sfc, Xmax, day, var_name, unit, title, tid):
+    fig = plt.figure(figsize=(20,7))
+    ax = plt.axes()
+# Vertical line to show end of day
+    ax.axvline(0,color = vert_col, linewidth = 3)
+    ax.axvline(24,color = vert_col, linewidth = 3)
+    ax.axvline(48,color = vert_col, linewidth = 3)
+## double fence    
+#### Morten
+    plt.plot(np.arange(0,Xmax), np.asarray(dofence_60)[:Xmax,(int(day)-1)], marker = 'o', markersize=20, 
+            color = 'k', linestyle = 'None', label = 'double fence')
+
+
+## ensemble member         
+    for ens_memb in range(2,10):
+        ax.plot(lead_time_sfc[ens_memb][:Xmax], variable[ens_memb][:Xmax], color = memb_col,
+           linestyle = '-', label='_nolegend_')
+    ax.plot(lead_time_sfc[1][:Xmax], variable[1][:Xmax], color = memb_col,
+           linestyle = '-', label = 'ensemble member')
+    ax.plot(lead_time_sfc[0][:Xmax], variable[0][:Xmax], 'k', linewidth = 4, label = 'best guess')
+
+
+
+### fine tuning
+    lgd = plt.legend(loc='upper left',fontsize=26)
+    ax.grid()
+
+# yaxis
+    ax.set_ylabel('%s %s %s' %(var_name[0], var_name[1], unit), fontsize = 30)
+    ax.set_ylim(-0.5,65)
+    T = np.arange(0,70,10)
+    ax.set_yticks(T)
+    ax.set_yticklabels(T,fontsize = 22)
+
+# xaxis
+    b = np.arange(0,Xmax+5,5)
+    ax.set_xlim(-0.5,Xmax-0.5)
+    ax.set_xlabel('lead time', fontsize = 30)
+    ax.set_xticks(np.arange(0,Xmax,5))
+
+    ax.set_xticklabels(b, fontsize = 22)
+# title
+    ax.set_title(title, fontsize=30, color ='k' )
+# tight layout
+    plt.tight_layout()
+      
