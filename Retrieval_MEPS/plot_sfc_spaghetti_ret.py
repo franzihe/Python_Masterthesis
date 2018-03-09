196517794852b353c656e682ebafe611e40c73e6
franzihe <franzihe@student.geo.uio.no>
2018-03-09 22:18:01 +0100

    same es in MEPS just in addtion with retrieved snow amount

---
+++ Retrieval_MEPS/plot_sfc_spaghetti_ret.py
@@ -0,0 +1,131 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+import plot_vertical as pvert 
+
+
+# In[2]:
+
+### Define colorbar colors
+champ = 255.
+blue = np.array([1,74,159])/champ           # for the date
+memb_col = np.array([99,99,99])/champ       # ensemble member color
+vert_col = np.array([197,197,197])/champ    # vertical line for day marker
+dofe = np.array([64,180,233])/champ         # color for double fence measurement
+
+
+
+
+# In[ ]:
+def spaghetti_sfc_dofe(lead_time_sfc, variable, dofence_60, #time_sfc, 
+              acc_ret,
+              Xmax, day, var_name, 
+              h_p18, m_p18, d_p18, y_p18, ini_day,
+            unit, title, tid, doublefence):
+    fig = plt.figure(figsize=(20,7))
+    ax = plt.axes()
+# Vertical line to show end of day
+    ax.axvline(0,color = vert_col, linewidth = 3)
+    ax.axvline(24,color = vert_col, linewidth = 3)
+    ax.axvline(48,color = vert_col, linewidth = 3)
+    if doublefence == 1:
+## double fence    
+#        plt.plot(np.arange(0,Xmax), np.asarray(dofence_60)[:,(int(day)-1)], marker = 'H', markersize=20, 
+ #           color = dofe, linestyle = 'None', label = 'double fence')
+        plt.plot(np.arange(0,Xmax), dofence_60, marker = 'H', markersize=20, 
+            color = dofe, linestyle = 'None', label = 'double fence')
+## ensemble member         
+    for ens_memb in range(2,10):
+        ax.plot(lead_time_sfc[ens_memb][:Xmax], variable[ens_memb][:Xmax], color = memb_col,
+           linestyle = '-', label='_nolegend_')
+    ax.plot(lead_time_sfc[1][:Xmax], variable[1][:Xmax], color = memb_col,
+           linestyle = '-', label = 'ensemble member')
+    ax.plot(lead_time_sfc[0][:Xmax], variable[0][:Xmax], 'k', linewidth = 4, label = 'best guess')
+    
+## retrieval
+    plt.plot(np.arange(0, np.asarray(acc_ret).shape[0]/60,1/60), acc_ret, linestyle = (0, (3, 1, 1, 1)), 
+         color = 'orange', label = 'retrieved snowfall',linewidth=6)
+### fine tuning
+    lgd = plt.legend(loc='upper left',fontsize=26)
+    ax.grid()
+
+# yaxis
+    ax.set_ylabel('%s %s %s' %(var_name[0], var_name[1], unit), fontsize = 30)
+    ax.set_ylim(-0.5,80)
+    T = np.arange(0,90,10)
+    ax.set_yticks(T)
+    ax.set_yticklabels(T,fontsize = 22)
+
+# xaxis
+    a = lead_time_sfc[0][0:48]
+#    ax.set_xlim(-0.5,Xmax+0.5)
+    ax.set_xlim(-0.5,Xmax-0.5)
+    ax.set_xlabel('time', fontsize = 30)
+    ax.set_xticks(np.arange(0,Xmax,6))
+    if tid == '18':
+#        dates = pvert.dates_plt_18(time_sfc)
+        dates = pvert.dates_plt_18(h_p18, m_p18, d_p18, y_p18, ini_day)
+    if tid == '00':
+#        dates = pvert.dates_plt(time_sfc)
+        dates = pvert.dates_plt_00(h_p18, m_p18, d_p18, y_p18, ini_day)
+    ax.set_xticklabels(dates, rotation = 25, fontsize = 22)
+# title
+    ax.set_title(title, fontsize=30, color =blue )
+# tight layout
+    plt.tight_layout()
+
+
+
+
+def spaghetti_sfc_dofe_Morten(lead_time_sfc, variable, dofence_60, #time_sfc, 
+                 Xmax, day, var_name, unit, title, tid):
+    fig = plt.figure(figsize=(20,7))
+    ax = plt.axes()
+# Vertical line to show end of day
+    ax.axvline(0,color = vert_col, linewidth = 3)
+    ax.axvline(24,color = vert_col, linewidth = 3)
+    ax.axvline(48,color = vert_col, linewidth = 3)
+## double fence    
+#### Morten
+    plt.plot(np.arange(0,np.asarray(dofence_60[:Xmax]).shape[0]), dofence_60[:Xmax], marker = 'o', markersize=20, 
+            color = 'k', linestyle = 'None', label = 'double fence')
+
+
+## ensemble member         
+    for ens_memb in range(2,10):
+        ax.plot(lead_time_sfc[ens_memb][:Xmax], variable[ens_memb][:Xmax], color = memb_col,
+           linestyle = '-', label='_nolegend_')
+    ax.plot(lead_time_sfc[1][:Xmax], variable[1][:Xmax], color = memb_col,
+           linestyle = '-', label = 'ensemble member')
+    ax.plot(lead_time_sfc[0][:Xmax], variable[0][:Xmax], 'k', linewidth = 4, label = 'best guess')
+
+
+
+### fine tuning
+    lgd = plt.legend(loc='upper left',fontsize=26)
+    ax.grid()
+
+# yaxis
+    ax.set_ylabel('%s %s %s' %(var_name[0], var_name[1], unit), fontsize = 30)
+    ax.set_ylim(-0.5,65)
+    T = np.arange(0,70,10)
+    ax.set_yticks(T)
+    ax.set_yticklabels(T,fontsize = 22)
+
+# xaxis
+    b = np.arange(0,Xmax+5,5)
+    ax.set_xlim(-0.5,Xmax-0.5)
+    ax.set_xlabel('lead time', fontsize = 30)
+    ax.set_xticks(np.arange(0,Xmax,5))
+
+    ax.set_xticklabels(b, fontsize = 22)
+# title
+    ax.set_title(title, fontsize=30, color ='k' )
+# tight layout
+    plt.tight_layout()
+      
